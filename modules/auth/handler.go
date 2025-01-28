6b860356b1c248e1cfa2293a17d536b73fc5a4e3
muhmmdfrd <mfarid2121@gmail.com>
2025-01-28 12:40:44 +0700

    feat: using base_response

--- modules/auth/handler.go
+++ modules/auth/handler.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"library-api/models/requests"
+	"library-api/models/responses"
 	"library-api/modules/users"
 	"library-api/utils"
 	"net/http"
@@ -21,37 +22,37 @@ func (h *AuthHandler) Login(c *gin.Context) {
     var request requests.AuthRequest
 
     if err := c.ShouldBindJSON(&request); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+        c.JSON(http.StatusBadRequest, responses.FailedResponse("Invalid input."))
         return
     }
 
     user, err := h.userRepo.Auth(request)
     if err != nil {
-        c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+        c.JSON(http.StatusUnauthorized, responses.FailedResponse("Invalid credentials."))
         return
     }
 
     token, err := utils.GenerateToken(uint(user.ID))
     if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+        c.JSON(http.StatusInternalServerError, responses.FailedResponse("Failed to generate token."))
         return
     }
 
-    // Simpan session di Redis
+    // Save session at Redis
     if err := utils.StoreSession(c.Request.Context(), uint(user.ID), token); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to store session"})
+        c.JSON(http.StatusInternalServerError, responses.FailedResponse("Failed to store session."))
         return
     }
 
-    c.JSON(http.StatusOK, gin.H{"token": token})
+    c.JSON(http.StatusOK, responses.SuccessResponse("success.", token))
 }
 
 func (h *AuthHandler) Logout(c *gin.Context) {
     userID := c.GetUint("user_id")
     if err := utils.DeleteSession(c.Request.Context(), userID); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete session"})
+        c.JSON(http.StatusInternalServerError, responses.FailedResponse("Failed to delete session."))
         return
     }
 
-    c.JSON(http.StatusOK, gin.H{"message": "Logged out successfully"})
+    c.JSON(http.StatusOK, responses.SuccessResponse("logged out", nil))
 }
